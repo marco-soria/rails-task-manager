09417dd46cbdcbd940b6c0f7489d05fdb660baff
Marco Soria <marco.soria@pucp.edu.pe>
2023-11-11 14:48:46 -0500

    avance

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #create
+  get "tasks/new",      to: "tasks#new",  as: :new_task
+  #Read one
+  get "tasks/:id", to: "tasks#show", as: :task
+  #Read all
+  get "tasks", to: "tasks#index"
 end
